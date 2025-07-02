2318e343c8590f7efba0282700886fc9268e7016
lamacias <la.macias@mavi.mx>
2025-07-02 12:27:44 -0600

    Update OtpReceivedInterface.java

--- src/android/OtpReceivedInterface.java
+++ src/android/OtpReceivedInterface.java
@@ -3,7 +3,7 @@ package com.outsystems.smsretriever;
 /**
  * Created on : Oct 10, 2019 Author : Paulo Camilo
  */
-public interface OtpReceivedInterface {
+public interface OtpReceivedInterface<String> {
   void onOtpReceived(String code);
   void onOtpTimeout();
 }
